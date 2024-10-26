828579771be44c696ff46efdc50fce412a0c3dae
Tristan Poland (Trident_For_U) <34868944+tristanpoland@users.noreply.github.com>
2024-10-26 00:44:58 -0400

    added nextJS site files

---
+++ next.config.ts
@@ -0,0 +1,29 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ui.aceternity.com',
+        port: '',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'assets.aceternity.com',
+        port: '',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        port: '',
+        pathname: '**',
+      },
+    ],
+  },
+  output: "export",
+};
+
+export default nextConfig;
